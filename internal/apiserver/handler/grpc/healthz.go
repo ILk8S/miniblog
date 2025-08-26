4767889bda49bbb22f7c7170257f39070b690461
王爽 <wangshuang6996@163.com>
2025-08-26 07:55:02 +0000

    小改动

--- internal/apiserver/handler/grpc/healthz.go
+++ internal/apiserver/handler/grpc/healthz.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"time"
 
-	v1 "github.com/wshadm/miniblog/pkg/api/apiserver/v1"
+	apiv1 "github.com/wshadm/miniblog/pkg/api/apiserver/v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // Healthz 服务健康检查.
-func (h *Handler) Healthz(ctx context.Context, rq *emptypb.Empty) (*v1.HealthResponse, error) {
-	return &v1.HealthResponse{
-		Status:    v1.ServiceStatus_Healthy,
+func (h *Handler) Healthz(ctx context.Context, rq *emptypb.Empty) (*apiv1.HealthResponse, error) {
+	return &apiv1.HealthResponse{
+		Status:    apiv1.ServiceStatus_Healthy,
 		Timestamp: time.Now().Format(time.DateTime),
 	}, nil
 }
